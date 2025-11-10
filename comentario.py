323c6b1556b3d3f2792d36523a0735e2e5f8b9b6
Vasco2510 <147677181+Vasco2510@users.noreply.github.com>
2025-11-09 22:02:16 -0500

    acutalizamos par asolver los erroes de labrole

--- comentario.py
+++ comentario.py
@@ -1,25 +1,13 @@
 import boto3
 import uuid
 import os
-import json # <-- Importar json
-
-# Crear clientes de AWS
-# fuera del handler
-# para reutilizar conexiones
-dynamodb = boto3.resource('dynamodb')
-s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     # Entrada (json)
     print(event)
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
-    
-    # Obtener nombres
-    # desde variables de entorno
     nombre_tabla = os.environ["TABLE_NAME"]
-    bucket_ingesta = os.environ["BUCKET_NAME"] # <-- NUEVO
-
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -29,32 +17,13 @@ def lambda_handler(event, context):
           'texto': texto
         }
     }
-    
-    # 1. Grabar en DynamoDB (como antes)
+    dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
-    response_dynamo = table.put_item(Item=comentario)
-    
-    # --- NUEVO: Grabar JSON en S3 ---
-    
-    # Definir nombre
-    # del archivo en S3
-    nombre_archivo_s3 = f"{uuidv1}.json" 
-    
-    # Subir el archivo
-    # al bucket S3
-    s3.Object(
-        bucket_ingesta, 
-        nombre_archivo_s3
-    ).put(
-        Body=json.dumps(comentario) # <-- Convertir dict a JSON
-    )
-    
-    # --- Fin de la sección S3 ---
-
+    response = table.put_item(Item=comentario)
     # Salida (json)
     print(comentario)
     return {
         'statusCode': 200,
         'comentario': comentario,
-        'response_dynamo': response_dynamo
+        'response': response
     }
